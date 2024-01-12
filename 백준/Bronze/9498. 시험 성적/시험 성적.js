4735eff1a12f5b49529c0e46f74fff6cda69f441
sy0725 <125878245+sy0725@users.noreply.github.com>
2024-01-12 18:34:48 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,17 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
+
+let score = input[0];
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
